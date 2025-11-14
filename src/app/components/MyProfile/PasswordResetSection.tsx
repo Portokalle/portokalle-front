88840623090229aa8642d42ed048d9e086a29ac9
lazari03 <alfonzlazri@yea.al>
2025-11-14 01:56:57 +0100

    register

--- src/app/components/MyProfile/PasswordResetSection.tsx
+++ src/app/components/MyProfile/PasswordResetSection.tsx
@@ -1,25 +1,31 @@
+'use client';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 interface PasswordResetSectionProps {
   handlePasswordReset: () => void;
   resetEmailSent: boolean;
 }
 
-const PasswordResetSection = ({ handlePasswordReset, resetEmailSent }: PasswordResetSectionProps) => (
-  <div className="mt-8 text-center">
-    <button
-      type="button"
-      onClick={handlePasswordReset}
-      className="text-black hover:text-orange-500 underline focus:outline-none"
-    >
-      Request to reset your password
-    </button>
-    {resetEmailSent && (
-      <p className="text-green-500 mt-4">
-        Password reset email sent successfully!
-      </p>
-    )}
-  </div>
-);
+const PasswordResetSection = ({ handlePasswordReset, resetEmailSent }: PasswordResetSectionProps) => {
+  const { t } = useTranslation();
+  
+  return (
+    <div className="mt-8 text-center">
+      <button
+        type="button"
+        onClick={handlePasswordReset}
+        className="text-black hover:text-orange-500 underline focus:outline-none"
+      >
+        {t('requestPasswordReset')}
+      </button>
+      {resetEmailSent && (
+        <p className="text-green-500 mt-4">
+          {t('passwordResetEmailSent')}
+        </p>
+      )}
+    </div>
+  );
+};
 
 export default PasswordResetSection;
